1514fde254e772f9b24298da03abd64aa0e9224e
Jeong, Heon <blmarket@gmail.com>
2010-02-03 23:07:01 +0900

    Added Auxillary tools solution and IconExtractor which is used for extracting icons from exe and dlls.

---
+++ AuxTools/IconExtractor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+namespace IconExtractor
+{
+    class Program
+    {
+        [DllImport("Shell32.dll")]
+        public extern static int ExtractIconEx(string libName, int iconIndex,
+            IntPtr[] largeIcon, IntPtr[] smallIcon, int nIcons);
+
+        static void Main(string[] args)
+        {
+            IntPtr[] LargeIcon = new IntPtr[100];
+            IntPtr[] SmallIcon = new IntPtr[100];
+
+            ExtractIconEx("xpsrchvw.exe", 0, LargeIcon, SmallIcon, 100);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Icon tmp = Icon.FromHandle(LargeIcon[i]);
+                tmp.ToBitmap().Save("test" + i + ".bmp");
+                tmp.Dispose();
+            }
+        }
+    }
+}
