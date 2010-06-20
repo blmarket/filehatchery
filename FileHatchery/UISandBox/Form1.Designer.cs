c4fc470b968f81886ac2a225698ba904c183a649
Neon <>
2010-06-20 11:39:29 +0900

    Timer를 붙여보는중...

--- FileHatchery/UISandBox/Form1.Designer.cs
+++ FileHatchery/UISandBox/Form1.Designer.cs
@@ -29,34 +29,72 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(189, 98);
+            this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(75, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "hehehehehe";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(107, 40);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(709, 260);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- FileHatchery/UISandBox/Form1.cs
+++ FileHatchery/UISandBox/Form1.cs
@@ -11,6 +11,8 @@ namespace UISandBox
 {
     public partial class Form1 : Form
     {
+        Timer tt = new Timer();
+
         public Form1()
         {
             InitializeComponent();
@@ -18,7 +20,24 @@ namespace UISandBox
 
         private void label1_Click(object sender, EventArgs e)
         {
+            tt.Interval = 1000;
+            tt.Enabled = true;
+            tt.Tick += new EventHandler(tt_Tick);
+        }
+
+        void tt_Tick(object sender, EventArgs e)
+        {
             label1.Hide();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label2.Hide();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label3.Hide();
         }
     }
 }
