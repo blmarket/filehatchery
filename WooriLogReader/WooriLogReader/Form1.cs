e1620b354dacbf81e09495a321128142b0e3f86a
blmarket@gmail.com <blmarket@gmail.com>
2010-10-21 11:29:13 +0900

    did some prototype code

--- WooriLogReader/WooriLogReader/Form1.cs
+++ WooriLogReader/WooriLogReader/Form1.cs
@@ -15,5 +15,34 @@ namespace WooriLogReader
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                var stream = new System.IO.StreamReader("asdf.xls", Encoding.Default);
+                string tmpx;
+                do
+                {
+                    tmpx = stream.ReadLine();
+                    if (tmpx.Contains("메모"))
+                    {
+                    }
+                    System.Diagnostics.Debug.WriteLine(tmpx);
+                } while (stream.EndOfStream == false);                
+                
+                stream.Close();
+            }
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.Load("asdf.xls", true);
+            HtmlAgilityPack.HtmlNode node = doc.DocumentNode;
+            var tmp = node.SelectNodes("//tr[@class='output_title']");
+            var par = tmp[0].ParentNode;
+            foreach (var iter in par.ChildNodes)
+            {
+                System.Diagnostics.Debug.WriteLine(iter);
+            }
+            System.Diagnostics.Debug.WriteLine("I am using dot net debugging");
+        }
     }
 }
